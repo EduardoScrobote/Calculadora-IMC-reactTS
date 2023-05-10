83343388bcd79b7ab132783e52f4a8cbcf87b599
Eduardo Rafael Scrobote <95055179+EduardoScrobote@users.noreply.github.com>
2023-05-10 17:20:23 -0300

    Update TumbsNSquare.tsx

--- src/components/squares/TumbsNSquare.tsx
+++ src/components/squares/TumbsNSquare.tsx
@@ -4,7 +4,7 @@ function TumbsNSquare() {
   return (
     <div>
       <div className='normalSquare'>
-        <img src="../../public/img/imagens/up.png" alt="" className='tumbsNSquare' />
+        <img src="https://i.ibb.co/93str8c/up.png" alt="" className='tumbsNSquare' />
         <h1>Normal</h1>
         <span>IMC está entre 18.5 e 24.9</span>
       </div>
