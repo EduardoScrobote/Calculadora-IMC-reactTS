575b2e5022db2fe38335d824ada36528882707a2
Eduardo Rafael Scrobote <95055179+EduardoScrobote@users.noreply.github.com>
2023-05-10 17:19:47 -0300

    Update TumbsOSquare.tsx

--- src/components/squares/TumbsOSquare.tsx
+++ src/components/squares/TumbsOSquare.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function TumbsOSquare() {
     return (
             <div className='obesidadeSquare'>
-                <img src="../../public/img/imagens/down.png" alt="" className='tumbsOSquare' />
+                <img src="https://i.ibb.co/31Z10Hk/down.png" alt="" className='tumbsOSquare' />
                 <h1>Obesidade</h1>
                 <span>IMC está entre 30 e 99</span>
             </div>
