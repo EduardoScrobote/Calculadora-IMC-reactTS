2d01a87694b2aa3bf3e3313e3c3012bb0b09a1e6
Eduardo Rafael Scrobote <95055179+EduardoScrobote@users.noreply.github.com>
2023-05-10 17:19:59 -0300

    Update TumbsSSquare.tsx

--- src/components/squares/TumbsSSquare.tsx
+++ src/components/squares/TumbsSSquare.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function TumbsSSquare() {
     return (
         <div className='sobrepesoSquare'>
-            <img src="../../public/img/imagens/down.png" alt="" className='tumbsSSqaure' />
+            <img src="https://i.ibb.co/31Z10Hk/down.png" alt="" className='tumbsSSqaure' />
             <h1>Sobrepeso</h1>
             <span>IMC está entre 24.9 e 30</span>
         </div>
