96e98d63ca329713b3c6976f6648998081ac16fc
Eduardo Rafael Scrobote <95055179+EduardoScrobote@users.noreply.github.com>
2023-05-10 17:19:27 -0300

    Update TumbsMSquare.tsx

--- src/components/squares/TumbsMSquare.tsx
+++ src/components/squares/TumbsMSquare.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function TumbsMSquare() {
     return (
                 <div className='magrezaSquare'>
-                    <img src="../../public/img/imagens/down.png" alt="" className='tumbsMSquare' />
+                    <img src="https://i.ibb.co/31Z10Hk/down.png" alt="" className='tumbsMSquare' />
                     <h1>Magreza</h1>
                     <span>IMC está entre 0 e 18.5</span>
                 </div>
